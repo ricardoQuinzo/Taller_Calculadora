033f4dd667460ab8273dbc5888f84dde8bfb377d
danielacevedo1994 <daniel.acevedo@epn.edu.ec>
2017-05-24 12:50:43 -0500

    Funcion_Multiplicacion

--- Taller_Calculadora.py
+++ Taller_Calculadora.py
@@ -9,7 +9,10 @@ def Resta():
     total = int(num1.get()) - int(num2.get())
     print(total)
 
-
+def Multiplicacion():
+    total = int(num1.get()) * int(num2.get())
+    print(total)
+	
     
 tk=Tk()
 ##definicion de variables a ingresar
@@ -22,9 +25,11 @@ num2.pack()
 
 suma = Button(tk, text="SUMA", command = Suma)
 resta = Button(tk, text="RESTA", command = Resta)
+multi= Button(tk, text="MULTIPLICACION", command = Multiplicacion)
 
 suma.pack()
 resta.pack()
+multi.pack()
 
 
 
