1d51c6e0a923deed480d4db26e3728a7ba261af1
22B10-PC\22B <estefania.aguilar@epn.edu.ec>
2017-05-24 13:05:32 -0500

    Funcion Division

--- Taller_Calculadora.py
+++ Taller_Calculadora.py
@@ -12,7 +12,14 @@ def Resta():
 def Multiplicacion():
     total = int(num1.get()) * int(num2.get())
     print(total)
-	
+
+def Division():
+    if int(num2.get()) == 0:
+         print("error")
+    else:
+        total = total = int(num1.get()) / int(num2.get())
+        print(total)
+ 
     
 tk=Tk()
 ##definicion de variables a ingresar
@@ -26,11 +33,12 @@ num2.pack()
 suma = Button(tk, text="SUMA", command = Suma)
 resta = Button(tk, text="RESTA", command = Resta)
 multi= Button(tk, text="MULTIPLICACION", command = Multiplicacion)
+division = Button(tk, text="DIVISION", command = Division)
 
 suma.pack()
 resta.pack()
 multi.pack()
-
+division.pack()
 
 
 
