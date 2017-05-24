595af366de3a6e84017eba6789af2934921f78df
22B05-PC\22B <pedro.munoz@epn.edu.ec>
2017-05-24 12:34:15 -0500

    resta

--- Taller_Calculadora.py
+++ Taller_Calculadora.py
@@ -5,11 +5,14 @@ def Suma():
     total = int(num1.get()) + int(num2.get())
     print(total)
 
+def Resta():
+    total = int(num1.get()) - int(num2.get())
+    print(total)
+
 
     
-    
-
 tk=Tk()
+##definicion de variables a ingresar
 ent1 = IntVar()
 ent2 = IntVar()
 num1 = Entry(tk, textvariable = ent1)
@@ -17,8 +20,11 @@ num2 = Entry(tk, textvariable = ent2)
 num1.pack()
 num2.pack()
 
-suma= Button(tk, text="SUMA", command = Suma)
+suma = Button(tk, text="SUMA", command = Suma)
+resta = Button(tk, text="RESTA", command = Resta)
+
 suma.pack()
+resta.pack()
 
 
 
